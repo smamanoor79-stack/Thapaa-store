27ebdb241ef88683581cbade778c0ae4c799157a
smamanoor <samamanoor79@gmail.com>
2026-06-01 22:01:01 +0500

    fix: updated homeproductcards import case

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 import products from "./api/products.json";
 
-import { showProductContainer } from "./homeProductCards";
+import { showProductContainer } from "./homeproductcards";
 
 // Define a function named `showProductContainer` that takes an array of products as input.
 
